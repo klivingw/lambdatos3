16d5f575e38171d0906bd5c3a092463763f81ac9
Kenneth L <klivingw+github@amazon.com>
2023-02-11 15:28:55 -0800

    Add GetObject function code

--- amplify/backend/function/lambdatos355b401fc/src/index.js
+++ amplify/backend/function/lambdatos355b401fc/src/index.js
@@ -9,6 +9,23 @@ Amplify Params - DO NOT EDIT */
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
+    s3.getObject(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+        /*
+        data = {
+         AcceptRanges: "bytes", 
+         ContentLength: 3191, 
+         ContentType: "image/jpeg", 
+         ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+         LastModified: <Date Representation>, 
+         Metadata: {
+         }, 
+         TagCount: 2, 
+         VersionId: "null"
+        }
+        */
+    });
     console.log(`EVENT: ${JSON.stringify(event)}`);
     return {
         statusCode: 200,
@@ -19,4 +36,10 @@ exports.handler = async (event) => {
     //  }, 
         body: JSON.stringify('Hello from Lambda!'),
     };
+};
+
+
+var params = {
+    Bucket: "examplebucket",
+    Key: "HappyFace.jpg"
 };
